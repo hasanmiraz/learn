a5d64667b8b75ef40ae9780d420bd34c603f17a5
Hasan Miraz <hasibul.miraz@gmail.com>
2025-05-22 17:15:56 +1000

    font and assets

--- WebDesign/Practice1/src/App.tsx
+++ WebDesign/Practice1/src/App.tsx
@@ -2,7 +2,11 @@
 
 const App = () => {
   return (
-    <div className="text-center">App</div>
+    <div className="">
+      <p className="font-zentry-regular">test1</p>
+      <p className="font-robert-medium">test3</p>
+      <p className="font-robert-regular">test3</p>
+    </div>
   )
 }
 
